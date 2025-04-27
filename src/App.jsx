e123e68e61412def7bfda45c01054f8b3de9eb76
nHung-k419 <leronalluom@gmail.com>
2025-04-27 18:38:59 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Game_assignment_test from './Components/Game_assignment_test'
+
+const App = () => {
+  return (
+    <div>
+      <Game_assignment_test/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Game_assignment_test.jsx
@@ -0,0 +1,124 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const Game_assignment_test = () => {
+    const [count, setCount] = useState(0);
+    const [valuePoints, setValuePoints] = useState('')
+    const [randomNumbers, setRandomNumbers] = useState([]);
+    const [positionedElements, setPositionedElements] = useState([]);
+    const [points, setPoints] = useState();
+    const [flagPoints, setFlagPoints] = useState(1)
+    const [isStart,setIsStart] = useState(1)
+    const containerRef = useRef(null);
+    const handleGetPoints = (e) => {
+        setValuePoints(e)
+
+
+    }
+    const handleClickRestart = () => {
+        generateRandomNumber(valuePoints)
+        setValuePoints('')
+        setCount(0)
+    }
+    const generateRandomNumber = (valuePoints) => {
+        const Numbers = []
+        for (let i = 1; i <= parseInt(valuePoints); i++) {
+            Numbers.push(i);
+        }
+        setRandomNumbers(Numbers)
+    }
+    const assignRandomPositions = () => {
+        if (!containerRef.current) return;
+
+        const containerWidth = containerRef.current.offsetWidth;
+        const containerHeight = containerRef.current.offsetHeight;
+
+        const positioned = randomNumbers.map(element => ({
+            value: randomNumbers[element - 1],
+            x: Math.random() * containerWidth,
+            y: Math.random() * containerHeight,
+        }));
+
+        setPositionedElements(positioned);
+    };
+    useEffect(() => {
+        assignRandomPositions();
+
+    }, [randomNumbers]);
+    const handleCheck = (keyItem) => {
+        setFlagPoints(flagPoints + 1)
+        setPoints(keyItem)   
+        if (flagPoints === keyItem) {
+            console.log(flagPoints);
+            const result = positionedElements.filter(item => item.value !== keyItem)
+            if(result.length === 0){
+                location.reload()
+            }
+            
+            const timeOutValid = setTimeout(() => {
+                setPositionedElements(result)
+            }, 700)
+            return () => {
+                clearInterval(timeOutValid)
+            }
+
+        }
+
+    }
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCount((prevCount) => prevCount + 1);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div style={{ marginLeft: '40px' }}>
+            <div>
+                <h1 style={positionedElements.length === 0 ? { color: 'green' } : { color: 'black' }}>{positionedElements.length === 0 ? 'ALL CLEARED' : 'LET PLAYS'}</h1>
+                <div>
+                    <p>Points : <span><input value={valuePoints} onChange={(e) => handleGetPoints(e.target.value)} type="text" /></span></p>
+                </div>
+                <p>Time : <span>{positionedElements.length !== 0 ? count : 0}s</span></p>
+                <button onClick={handleClickRestart}>Restart</button>
+            </div>
+            <div ref={containerRef} style={{ width: '500px', height: '400px', border: '1px solid gray', marginTop: '20px', position: 'relative' }}>
+                {positionedElements.map((item, index) => (
+                    <div onClick={() => { flagPoints === item.value ? handleCheck(item.value) : '' }} style={points === item.value ? {
+                        width: '30px',
+                        height: '30px',
+                        borderRadius: '50%',
+                        textAlign: 'center',
+                        lineHeight: '30px',
+                        border: '1px solid gray',
+                        position: 'absolute',
+                        left: `${item.x}px`,
+                        top: `${item.y}px`,
+                        cursor: 'pointer',
+                        backgroundColor: 'red',
+                        zIndex: '1',
+                        transition: 'background-color 0.5s ease-in-out',
+
+                    } : {
+                        width: '30px',
+                        height: '30px',
+                        borderRadius: '50%',
+                        textAlign: 'center',
+                        lineHeight: '30px',
+                        border: '1px solid gray',
+                        position: 'absolute',
+                        left: `${item.x}px`,
+                        top: `${item.y}px`,
+                        cursor: 'pointer',
+
+                    }} >{item.value}</div>
+                ))}
+            </div>
+        </div >
+    )
+}
+
+export default Game_assignment_test
